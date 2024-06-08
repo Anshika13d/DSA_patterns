9b5bef612e4e3fe97744645e84d266fc7c34a49c
Anshika <anshika1366@gmail.com>
2024-06-09 00:26:47 +0530

    modulo

---
+++ Subarrays/Continous_subarray_sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            int rem = sum % k;
+            if(!map.containsKey(rem)){
+                map.put(rem, i);
+            }
+            else{
+                int ind = map.get(rem);
+                int size = i - ind;
+                if(size > 1){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
