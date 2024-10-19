2c796e6ba05a73c3dbc835a4c57f19955bbc4bef
Anshika <116324398+Anshika13d@users.noreply.github.com>
2024-10-19 20:59:30 +0530

    cycle in a graph

--- Graph/Cycle/Cycle_in_undirected_graph.java
+++ Graph/Cycle/Cycle_in_undirected_graph.java
@@ -1,3 +1,5 @@
+//using DFS
+
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(ArrayList<ArrayList<Integer>> adj){
@@ -25,4 +27,57 @@ class Solution {
         return false;
     }
 
+}
+
+
+
+//using BFS
+
+class Pair{
+    int node;
+    int parent;
+    Pair(int node, int parent){
+        this.node = node;
+        this.parent = parent;
+    }
+}
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(ArrayList<ArrayList<Integer>> adj){
+    	int n = adj.size();
+        boolean[] vis = new boolean[n];
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                if(bfs(adj, vis, i)) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean bfs(ArrayList<ArrayList<Integer>> adj, boolean[] vis, int curr){
+
+        Queue<Pair> q = new LinkedList<>();
+        
+        q.offer(new Pair(curr, -1));
+        
+        vis[curr] = true;
+        
+        while(!q.isEmpty()){
+            Pair pair = q.poll();
+            int node = pair.node;
+            int parent = pair.parent;
+            
+            for(int neigh: adj.get(node)){
+                if(neigh == parent) continue;
+                if(vis[neigh]) return true;
+                q.offer(new Pair(neigh, node));
+                vis[neigh] = true;
+            }
+        }
+        
+        return false;
+    }
 }
